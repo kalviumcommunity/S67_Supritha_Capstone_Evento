300c1390d72a353e1aaea83149b75e6506c16909
supritha-rajashekar <supritha.rajashekar.s67@kalvium.community>
2025-04-20 23:08:57 +0530

    Established relationship between multiple database entities

--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,28 +1,28 @@
-// const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 
-// const productSchema = new mongoose.Schema({
-//     name : {
-//         type : String,
-//         required : true
-//     },
-//     price : {
-//         type : Number,
-//         min : 0,
-//         required : true
-//     },
-//     quantity : {
-//         type : Number,
-//         required : true
-//     },
-//     image : {
-//         type : [String],
-//         default : [],
-//         required : true
-//     },
-//     reviews : [{
-//         type : mongoose.Schema.Types.ObjectId,
-//         ref : 'Review'
-//     }]
-// })
+const productSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true
+    },
+    price : {
+        type : Number,
+        min : 0,
+        required : true
+    },
+    quantity : {
+        type : Number,
+        required : true
+    },
+    image : {
+        type : [String],
+        default : [],
+        required : true
+    },
+    reviews : [{
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'Review'
+    }]
+})
 
-// module.exports = mongoose.model('Product', productSchema)
+module.exports = mongoose.model('Product', productSchema)
