61f3522b7658e05daa2a74fada363ec393d7bd11
supritha-rajashekar <supritha.rajashekar.s67@kalvium.community>
2025-04-20 22:59:33 +0530

    Added POST route for customer

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const connectDB = require('./config/db')
-const customerRoute = require('./routes/customerRoutes')
+const customerRouter = require('./routes/customerRoutes')
 require('dotenv').config()
 
 const app = express()
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
     res.send("Welcome to the Home Page")
 })
 
-app.use('/customers', customerRoute)
+app.use('/customers', customerRouter)
 
 app.listen(PORT, () => {
     console.log(`The server is successfully running on http://localhost:${PORT}`)
