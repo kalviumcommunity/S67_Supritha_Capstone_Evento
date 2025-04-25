c8c9f5a1ccf123c706705444f2fa1412cbedd576
supritha-rajashekar <supritha.rajashekar.s67@kalvium.community>
2025-04-25 09:53:26 +0530

    Updated auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -7,7 +7,7 @@ const authMiddleware = (req,res,next)=>{
         if(!authHeader || !authHeader.startsWith("Bearer ")) return res.status(401).json({message: "Unauthorised: no token provided"});
 
         const token = authHeader.split(" ")[1];
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const decoded = jwt.verify(token, process.env.SECRET_KEY);
 
         req.user = decoded;
         next();
